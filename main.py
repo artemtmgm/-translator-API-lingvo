adce0b6cb013af25b2e19aaae1e66bd218adbed2
artemtmgm <artemtmgm@gmail.com>
2021-09-25 15:34:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+URL_AUTH = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+URL_TRANSLATE = 'https://developers.lingvolive.com/api/v1/Minicard'
+KEY = 'MTkzOWRmZDQtMTk1MS00YjBmLTliOTctZGYwNjM1ZGYyZmU4OmNkOTdmZWIwZjkzNzQ4NDhiZjY0YzdiMTBiYzM2NDhl'
+
+headers_auth = {'Authorization': 'Basic '+KEY}
+auth = requests.post(URL_AUTH,headers=headers_auth)
+
+if auth.status_code == 200:
+    token = auth.text
+
+    while True:
+        word = input('Введите слово для перевода: ')
+        if word:
+            headers_translate = {
+                'Authorization': 'Bearer ' + token
+            }
+            params = {
+                'text': word,
+                'srcLang': 1033,
+                'dstLang': 1049
+            }
+            r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
+            res = r.json()
+            try:
+                print(res['Translation']['Translation'])
+            except:
+                print('Не найдено варианта перевода.')
+else:
+    print('Error!')
